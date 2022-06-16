d85eef1840e9c2f23ff08875d1003fc62e1a9d1e
Emmanuel Olawoore <101262045+DevBadAss@users.noreply.github.com>
2022-06-16 12:41:06 +0100

    Changes

--- uuid.js
+++ uuid.js
@@ -1,16 +1,29 @@
 /**
+ * Generates ID
+ * @param {Number} length length of unique id
+ * @returns ID
+ * @author Olawoore Emmanuel Collins
+ */
+function ID(length) {
+
+    let letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()";
+    let id = "";
+
+    for (let i = 0; i < length; i++) {
+        let generate = letters[Math.floor(Math.random() * 64)];
+        id += generate;
+    }
+    return id;
+
+}
+
+/**
  * Generates unique ID
  * @param {Number} length length of unique id
  * @returns Unique ID
  * @author Olawoore Emmanuel Collins
  */
-export default function UUID(length) {
-    const chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890123456789#$%";
-    var ID = "";
-    for (let i = 0; i <= length; i++) {
-        var rand = Math.floor(Math.random() * chars.length);
-        ID += chars.substring(rand, rand + 1) + "-" + chars.substring(rand, rand + 2) + "-" + chars.substring(rand, rand + 3);
-    }
 
-    return ID;
+export default function uuid(length) {
+    return ID(length) + "-" + ID(length) + "-" + ID(length)
 }
